db551feb370b37a3e7a99c011dd1e6fd0c67f579
Ubaidillah Ariq <ubaidillahariq.rea@ruangguru.com>
2022-12-01 17:57:06 +0700

    add jwt for subscription

--- src/service/subscription.js
+++ src/service/subscription.js
@@ -1,16 +1,16 @@
 import axios from "../axios";
 
-export const getSubs = async () => {
-    const result = await axios.get(`subscription`);
+export const getSubs = async (config) => {
+    const result = await axios.get(`subscription`, config);
     return result;
 }
 
-export const approval = async (data) => {
-    const result = await axios.post(`subscription/approval`, data );
+export const approval = async (data, config) => {
+    const result = await axios.post(`subscription/approval`, data, config );
     return result;
 }
 
-export const reject = async (data) => {
-    const result = await axios.post(`subscription/reject`, data );
+export const reject = async (data, config) => {
+    const result = await axios.post(`subscription/reject`, data, config );
     return result;
 }
